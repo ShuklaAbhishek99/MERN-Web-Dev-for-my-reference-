2ded68d243da00ecbee55b6f7e562423f4125d96
abhishek <abhishekshukla821@gmail.com>
2023-05-25 00:11:58 +0530

    tut52-56

---
+++ JavaScript/tut53.js
@@ -0,0 +1,25 @@
+console.log("This is tutorial 53")
+
+// this function doesn't return any value
+function greet(name, greetText = "Greetings from JavaScript"){
+        console.log(greetText + " " + name);
+        console.log(name + " is a good boy.");
+}
+
+// This function return value
+function sum(a, b, c){
+    return a+b+c;
+}
+
+let name1 = "Abhishek";
+let name2 = "Shukla";
+let name3 = "abhi";
+let name4 = "shu";
+let = greetText = "Good Morning";
+
+greet(name1, greetText);
+greet(name2, greetText);
+greet(name3, greetText);
+greet(name4, greetText);
+
+console.log(sum(1, 2, 23));
