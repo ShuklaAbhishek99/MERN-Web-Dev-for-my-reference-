4a61b98ea6f46216678f230f61c81ad408ce0245
abhishek <abhishekshukla821@gmail.com>
2023-05-30 09:06:27 +0530

    tut69 npm

---
+++ Backend with Node.js/tut69 npm/tut69.js
@@ -0,0 +1,15 @@
+// ^ means the Exact package version
+// ~ means to install patches along with it(i.e 1.0. 2 )/installs the package
+// > Means to install latest version of that module
+// • npm view slugify version - to view version.
+// • npm i - to install deleted module folder
+// • npm --version - to view the version.
+// npm init initialize the package creation and then fill all the details
+// npm install slugify --> installs it, can also use npm i 
+// npm uninstall express --> uninstalls it
+// npm install slugify@1.4.5 --> installs a particular version of a package
+// 1.4.5 => major.minor.patch
+// npm install nodemon --save-dev => command line utility
+// npm install nodemon --global => This installs globally so you can use it in any function
+// nodemon helps to reflects changes instantly
+console.log("This is 69");
